67ba9e59322d75bf253654f4d2ef3845475f7141
Vinicio Almeida <almeida.vinicio@gmail.com>
2023-10-03 21:46:17 -0300

    plot

--- var.py
+++ var.py
@@ -3,14 +3,14 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 assets = ['ABEV3.SA', 'CIEL3.SA', 'COGN3.SA', 'EGIE3.SA', 'KLBN11.SA', 
           'LWSA3.SA', 'MGLU3.SA', 'MRFG3.SA', 'MULT3.SA', 'PETZ3.SA']
 weights = np.array([0.10, 0.10, 0.10, 0.10, 0.10,
                     0.10, 0.10, 0.10, 0.10, 0.10])
 
-start = '2007-01-01'
+start = '2022-01-01'
 end = '2023-09-06'
 
 portfolio = yf.download(assets, start = start, end = end)['Adj Close']
@@ -18,7 +18,6 @@ portfolio.head()
 
 returns = portfolio.pct_change()
 returns.head()
-
 portfolioreturn = (returns * weights).sum(axis=1)
 portfolioreturn.head()
 
@@ -27,17 +26,19 @@ portfolioreturndf = pd.DataFrame()
 portfolioreturndf["Returns"] = portfolioreturn
 portfolioreturndf.head()
 
-# histogram
-fig = plt.figure()
-ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
-portfolioreturndf['Returns'].plot.hist(bins = 80)
-ax1.set_xlabel("Daily returns %")
-ax1.set_ylabel("Percent")
-ax1.set_title("Portfolio daily returns data")
-ax1.text(-0.35,200,"Extreme Low\nreturns")
-ax1.text(0.25,200,"Extreme High\nreturns")
-plt.show()
-
 # V@R
-var95 = np.nanpercentile(portfolioreturndf, 5)
-var95*100
+confidence_level = 0.95
+historical_var = np.nanpercentile(portfolioreturndf, (1-confidence_level)*100)
+print(f"Historical VaR at {confidence_level*100}% confidence level: {historical_var:.4f}")
+
+#Plot
+plt.hist(portfolioreturndf, bins=10, density=True, alpha=0.6, color='g', label='Returns')
+# Add a vertical line to mark the VaR
+plt.axvline(x=historical_var, color='r', linestyle='--', label=f'{confidence_level*100}% VaR')
+# Add labels and legend
+plt.xlabel('Returns')
+plt.ylabel('Frequency')
+plt.legend()
+# Show the plot
+plt.title('Historical Value at Risk (VaR) Calculation')
+plt.show()
